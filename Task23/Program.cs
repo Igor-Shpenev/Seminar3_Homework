6eb28ff2efdc30412ae570113431a36e67eb997c
Igor Shpenev <vdvshneek@yandex.ru>
2022-10-25 14:50:06 +0500

    Completed task number 23

---
+++ Task23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void CubeNumber(int num)
+{
+    /*for (int i = 1; i<= num; i++)
+    {
+        Console.Write($"{i * i * i} ");   // Решение через цикл for
+    }*/
+
+    int x = 1;
+    while (x <= num)
+    {
+        int result = x * x * x;
+        x++;
+        Console.Write($"{result} ");
+    }
+    
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+CubeNumber(number);
